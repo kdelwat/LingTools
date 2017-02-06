32f7ea1f5b8f5a31f48d9d30169a9c7ee0fc564f
Cadel Watson <cadel@cadelwatson.com>
2017-02-07 10:46:42 +1100

    Comment NavigationBar

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -2,8 +2,19 @@ import React from 'react';
 import { StyleSheet, css } from 'aphrodite';
 import { Link, browserHistory } from 'react-router';
 
+/* NavigationBar is a component which creates a navigation bar. It takes the
+current URL as a prop. If the current URL is the index, a full navigation bar
+is displayed, with links from a prop shown. Otherwise, the name of the current
+route is shown and a back button displayed.
 
+Props:
+	- url (required): a string of the current URL.
+	- links (required): an array of link objects, each of which must contain a
+	'url' key representing the url and a 'text' key representing the link's
+	display text.
+*/
 function NavigationBar(props) {
+	// Render the index navigation if on the root url
 	if (props.url === '/') {
 		return (
 			<div className={css(styles.navigationBar)}>
@@ -15,6 +26,7 @@ function NavigationBar(props) {
 		);
 	}
 
+	// Otherwise, render the normal navigation bar with a back link
 	return (
 		<div className={css(styles.navigationBar)}>
 			<a onClick={() => browserHistory.goBack()}>Back</a>
