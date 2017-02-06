db904783311374d0c98626f8bd3bdeb2eb283abb
Cadel Watson <cadel@cadelwatson.com>
2017-02-06 16:16:59 +1100

    Fix warning during HMR about changing Routes

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import Home from './Home';
 import GrammarGen from './GrammarGen';
 
 const App = () => (
-	<Router history={browserHistory}>
+	// Adding a random key to the Router prevents the hot module reloader from
+	// complaining about Routes being changed.
+	<Router key={Math.random()} history={browserHistory}>
 		<Route path="/" component={NavigationContainer}>
 			<IndexRoute component={Home} />
-			<Route path="/grammargen" component={GrammarGen} />
+			<Route path="grammargen" component={GrammarGen} />
 		</Route>
 	</Router>
 );
