42676beb7ed84913ba3b231ce11e6de6f119ebf3
Cadel Watson <cadel@cadelwatson.com>
2017-02-07 09:56:09 +1100

    Add list item promotion and demotion

--- src/components/OrderableListView.js
+++ src/components/OrderableListView.js
@@ -8,7 +8,10 @@ class OrderableListView extends React.Component {
 			items: props.initial ? props.initial : [],
 		};
 
+		this.renderListItem = this.renderListItem.bind(this);
 		this.addItem = this.addItem.bind(this);
+		this.promoteItem = this.promoteItem.bind(this);
+		this.demoteItem = this.demoteItem.bind(this);
 	}
 
 	addItem() {
@@ -20,9 +23,45 @@ class OrderableListView extends React.Component {
 		});
 	}
 
-	renderListItem(listItem) {
+	promoteItem(index) {
+		if (index === 0) {
+			return;
+		}
+
+		const { items } = this.state;
+
+		const promotedItem = items[index];
+		items[index] = items[index - 1];
+		items[index - 1] = promotedItem;
+
+		this.setState({
+			items,
+		});
+	}
+
+	demoteItem(index) {
+		const { items } = this.state;
+
+		if (index === items.length - 1) {
+			return;
+		}
+
+		const demotedItem = items[index];
+		items[index] = items[index + 1];
+		items[index + 1] = demotedItem;
+
+		this.setState({
+			items,
+		});
+	}
+
+	renderListItem(listItem, index) {
 		return (
-			<div className={css(styles.item)}>{listItem}</div>
+			<div className={css(styles.item)}>
+				{listItem} {index}
+				<a onClick={() => this.promoteItem(index)}>Promote</a>
+				<a onClick={() => this.demoteItem(index)}>Demote</a>
+			</div>
 		);
 	}
 
