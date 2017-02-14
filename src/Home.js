657497047f5c2d68b3d88bf6acb49c0d0fbf9528
Cadel Watson <cadel@cadelwatson.com>
2017-02-14 16:31:50 +1100

    Add homepage text

--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,20 @@
 import React from 'react';
 
 import Block, { Container } from './components/Layout';
-import { NotificationArea, addNotification } from './components/Notifications';
-
 
 const Home = () => (
 	<Container>
-		<NotificationArea />
 		<Block width={'100%'}>
-			<h1>Welcome to the site!</h1>
-			<button onClick={() => addNotification('This is quite a long error message', 'warning')} />
-			<button onClick={() => addNotification('An info occured', 'info')} />
-
-		</Block>
-		<Block width={'50%'} mobileWidth={'100%'}>
-			<h2>Hello, world!</h2>
-		</Block>
-		<Block width={'50%'} mobileWidth={'100%'}>
-			<h2>How you doin?</h2>
+			<div className="content">
+				<h1>Welcome to LingTools!</h1>
+				<p>This site is a collection of tools for linguistics and conlanging. If you encounter any problems at all, please do not hesitate to contact me via <a href="mailto:cadel@cadelwatson.com">email</a>, or create an issue in the <a href="https://github.com/kdelwat/lingtools">GitHub repository</a>.</p>
+				<p>Currently, the included tools are:
+					<ul>
+						<li><strong>Coda</strong>: creates professional-looking
+						reference grammars from Markdown sources.</li>
+					</ul>
+				</p>
+			</div>
 		</Block>
 	</Container >
 );
