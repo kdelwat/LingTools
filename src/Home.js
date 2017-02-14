eb4b2d1f8aeba9f50d6bbcefdffc2c8dd87dc2ef
Cadel Watson <cadel@cadelwatson.com>
2017-02-15 09:28:16 +1100

    Fix nested list issue

--- src/Home.js
+++ src/Home.js
@@ -8,12 +8,11 @@ const Home = () => (
 			<div className="content">
 				<h1>Welcome to LingTools!</h1>
 				<p>This site is a collection of tools for linguistics and conlanging. If you encounter any problems at all, please do not hesitate to contact me via <a href="mailto:cadel@cadelwatson.com">email</a>, or create an issue in the <a href="https://github.com/kdelwat/lingtools">GitHub repository</a>.</p>
-				<p>Currently, the included tools are:
-					<ul>
-						<li><strong>Coda</strong>: creates professional-looking
+				<p>Currently, the included tools are:</p>
+				<ul>
+					<li><strong>Coda</strong>: creates professional-looking
 						reference grammars from Markdown sources.</li>
-					</ul>
-				</p>
+				</ul>
 			</div>
 		</Block>
 	</Container >
